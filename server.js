236bb66ed1ee96210e32de81c351fc0f8297ff49
rumonpro <rubaitsiamlrumon@gmail.com>
2026-06-25 11:36:09 +0600

    update

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ connectDB();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '15mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
